32a26958771673b251ed147b514cbf65ca0511b8
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-26 16:17:28 -0300

    01 - Implemente o módulo de acesso à api do Mercado Livre

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,28 @@
 export async function getCategories() {
   // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(url);
+  const dataCat = await response.json();
+  // console.log(dataCat);
+  return dataCat;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (query) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    const response = await fetch(url);
+    const dataProd = await response.json();
+    // console.log(dataProd);
+    return dataProd;
+  }
+  if (categoryId) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    const response = await fetch(url);
+    const dataCatId = await response.json();
+    // console.log(dataCatId);
+    return dataCatId;
+  }
 }
+
+console.log(getCategories());
