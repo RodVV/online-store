68bbb9fd1dd12771eba5e12a014f091aa3781a16
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-05-03 18:55:29 -0300

    req 11 - add form e suas formas

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import { addForm, getForm } from '../services/storage';
+
+export default class ReviewForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nota: '0',
+      textArea: '',
+      email: '',
+      reviews: [],
+    };
+  }
+
+  componentDidMount() {
+    const loadForm = getForm() !== null ? getForm() : [];
+    // console.log(loadForm);
+    this.setState({ reviews: loadForm });
+  }
+
+  notaChange = (value) => {
+    this.setState({ nota: value });
+  };
+
+  commentChange = ({ target: { value } }) => {
+    this.setState({ textArea: value });
+  };
+
+  emailChange = ({ target: { value } }) => {
+    this.setState({ email: value });
+  };
+
+  submitBtn = (e) => {
+    e.preventDefault();
+    const { nota, textArea, email } = this.state;
+    const obj = { nota, textArea, email };
+    addForm(obj);
+    this.setState((prevState) => ({
+      reviews: [...prevState.reviews, obj],
+    }));
+  };
+
+  render() {
+    const { nota, textArea, email, reviews } = this.state;
+    // console.log(reviews);
+    return (
+      <>
+        <form onSubmit={ this.submitBtn }>
+          {/*
+        avaliação 1-5 -> radio buttons (required)
+          -> data-testid="${index}-rating"
+          -> index = nota
+          */}
+          <div>
+            <label htmlFor="nota1">
+              <input
+                name="nota"
+                type="radio"
+                id="nota1"
+                onChange={() => this.notaChange(1)}
+                data-testid="1-rating"
+              />
+              1
+            </label>
+            <label htmlFor="nota2">
+              <input
+                name="nota"
+                type="radio"
+                id="nota2"
+                onChange={() => this.notaChange(2)}
+                data-testid="2-rating"
+              />
+              2
+            </label>
+            <label htmlFor="nota3">
+              <input
+                name="nota"
+                type="radio"
+                id="nota3"
+                onChange={() => this.notaChange(3)}
+                data-testid="3-rating"
+              />
+              3
+            </label>
+            <label htmlFor="nota4">
+              <input
+                name="nota"
+                type="radio"
+                id="nota4"
+                onChange={() => this.notaChange(4)}
+                data-testid="4-rating"
+              />
+              4
+            </label>
+            <label htmlFor="nota5">
+              <input
+                name="nota"
+                type="radio"
+                id="nota5"
+                onChange={() => this.notaChange(5)}
+                data-testid="5-rating"
+              />
+              5
+            </label>
+          </div>
+          {/*
+        comentários -> text area (opcional)
+          -> data-testid="product-detail-evaluation"
+        exibir prev reviews
+        */}
+          <label htmlFor="Comment">
+            <input
+              type="text"
+              placeholder="digite aqui seu comentário"
+              name="Comment"
+              data-testid="product-detail-evaluation"
+              value={textArea}
+              onChange={(e) => this.commentChange(e)}
+            />
+          </label>
+          {/*
+        email -> data-testid="product-detail-email"
+        */}
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              data-testid="product-detail-email"
+              value={email}
+              onChange={(e) => this.emailChange(e)}
+            />
+          </label>
+          {/*botão submit -> data-testid="submit-review-btn"
+        localStorage
+        */}
+          <button type="submit" data-testid="submit-review-btn">
+            Enviar
+          </button>
+        </form>
+        <div>
+          {reviews.map((review) => (
+            <div key={review.email}>
+              <p data-testid="product-detail-email">{review.email}</p>
+              <br />
+              <p data-testid={`${review.nota}-rating`}>{review.nota}</p>
+              <br />
+              <p data-testid="product-detail-evaluation">{review.textArea}</p>
+              <br />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
